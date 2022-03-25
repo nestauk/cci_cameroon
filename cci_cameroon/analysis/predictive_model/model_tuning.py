3b0fad15f934fa30417a443f8f4214f6c09ac8be
Isabel <isabel.stewart@nesta.org.uk>
2022-03-25 09:09:42 +0000

    updating predictive files

--- cci_cameroon/analysis/predictive_model/model_tuning.py
+++ cci_cameroon/analysis/predictive_model/model_tuning.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -16,18 +17,13 @@
 
 # %% [markdown]
 # ## Model tuning
-#
-# To test:
-# - Different language models
-# - Different values for K (KNN)
-# - Different models
-# - Model hyper-parameters
 
 # %%
 # Read in libraries
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 import pandas as pd
+import numpy as np
 from sentence_transformers import SentenceTransformer
 from sklearn.preprocessing import MultiLabelBinarizer
 import os
@@ -38,13 +34,13 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.multiclass import OneVsOneClassifier
+from sklearn.multioutput import MultiOutputClassifier
 from sklearn.svm import SVC
+from collections import Counter
 
 # Project modules
 import cci_cameroon
-
-# %%
+from cci_cameroon.pipeline import process_workshop_data as pwd
 
 # %%
 # Set directory
@@ -69,6 +65,27 @@ def perform_grid_search(pipe, score, parameter_grid):
 
 
 # %%
+def save_cm_plots(cm, model_type):
+    """
+    Save cm plot for each class for chosen model type. Note: model type needs to match folder name in outputs/figures.
+    """
+    # Loop through codes and save cm plot to outputs/figures sub-folder.
+    for i in range(0, len(codes)):
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm[i])
+        disp.plot()
+        plt.title(codes[i].replace("_", " "), pad=20)
+        plt.tight_layout()
+        plt.savefig(
+            f"{project_directory}/outputs/figures/predictive_models/cm/"
+            + model_type
+            + "/"
+            + codes[i].replace("/", "")
+            + "_cm.png",
+            bbox_inches="tight",
+        )
+
+
+# %%
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 # %%
@@ -87,8 +104,51 @@ y_test = pd.read_excel(
 )["category_id"]
 
 # %%
+no_response_train = pd.read_excel(
+    f"{project_directory}/outputs/data/data_for_modelling/no_response_train.xlsx",
+    index_col="id",
+)["comment"]
+
+no_response_test = pd.read_excel(
+    f"{project_directory}/outputs/data/data_for_modelling/no_response_test.xlsx",
+    index_col="id",
+)["comment"]
+
+# %%
+no_response_train
+
+# %%
+y_nr_train = np.array([[0] * 8] * 120)
+y_nr_test = np.array([[0] * 8] * 30)
+
+# %%
 y_train = y_train.apply(literal_eval)
 y_test = y_test.apply(literal_eval)
+
+# %%
+# Transform Y into multilabel format
+mlb = MultiLabelBinarizer()
+y_train = mlb.fit_transform(y_train)
+y_test = mlb.transform(y_test)
+codes = list(mlb.classes_)  # Codes list
+
+# %%
+y_train = np.concatenate((y_train, y_nr_train))
+y_test = np.concatenate((y_test, y_nr_test))
+
+# %%
+X_train = X_train.append(no_response_train, ignore_index=False)
+X_test = X_test.append(no_response_test, ignore_index=False)
+
+# %%
+# Combine files and re-shuffle
+X_train.head(1)
+
+# %%
+from sklearn.utils import shuffle
+
+X_train, y_train = shuffle(X_train, y_train, random_state=1)
+X_test, y_test = shuffle(X_test, y_test, random_state=1)
 
 # %%
 # Get language models
@@ -105,22 +165,15 @@ X_train_embeddings_fr = model_fr.encode(list(X_train))
 X_test_embeddings_fr = model_fr.encode(list(X_test))
 
 # %%
-# Transform Y into multilabel format
-mlb = MultiLabelBinarizer()
-y_train = mlb.fit_transform(y_train)
-y_test = mlb.transform(y_test)
-codes = list(mlb.classes_)  # Codes list
-
-# %%
-codes
+y_train.shape
 
 # %%
 # Models
 knn = KNeighborsClassifier(n_neighbors=7)
 rf = RandomForestClassifier(random_state=1)
 dt = DecisionTreeClassifier(random_state=1)
-nb = GaussianNB()
-svm = OneVsOneClassifier(SVC())
+nb = MultiOutputClassifier(GaussianNB(), n_jobs=-1)
+svm = MultiOutputClassifier(SVC(), n_jobs=-1)
 
 # %%
 # Pipelines
@@ -208,9 +261,6 @@ best_score_fr_dt = search_dt.best_score_
 best_params_fr_dt = search_dt.best_params_
 
 # %%
-svm.estimator.get_params().keys()
-
-# %%
 # %%capture
 # Fit to train and get optimum score and parameters
 search_svm.fit(X_train_embeddings, y_train)
@@ -219,6 +269,19 @@ best_params_svm = search_svm.best_params_
 search_svm.fit(X_train_embeddings_fr, y_train)
 best_score_fr_svm = search_svm.best_score_
 best_params_fr_svm = search_svm.best_params_
+
+# %%
+# %%capture
+# Fit to train and get optimum score and parameters
+search_nb.fit(X_train_embeddings, y_train)
+best_score_nb = search_nb.best_score_
+best_params_nb = search_nb.best_params_
+search_nb.fit(X_train_embeddings_fr, y_train)
+best_score_fr_nb = search_nb.best_score_
+best_params_fr_nb = search_nb.best_params_
+
+# %% [markdown]
+# ### Without extra comments
 
 # %%
 ## KNN
@@ -254,19 +317,202 @@ print("Optimum parameters:")
 print(best_params_fr_dt)
 
 # %%
-y_pred = search.best_estimator_.predict(X_test_embeddings_fr)
+## NB
+print("Best scores from multi-lingual model:")
+print(best_score_nb)
+print("Optimum parameters:")
+print(best_params_nb)
+print("Best scores from French language model:")
+print(best_score_fr_nb)
+print("Optimum parameters:")
+print(best_params_fr_nb)
 
 # %%
-# Create confusion matrix from the best performing model
-cm_KNN = multilabel_confusion_matrix(y_test, y_pred)
+## SVM
+print("Best scores from multi-lingual model:")
+print(best_score_svm)
+print("Optimum parameters:")
+print(best_params_svm)
+print("Best scores from French language model:")
+print(best_score_fr_svm)
+print("Optimum parameters:")
+print(best_params_fr_svm)
+
+# %% [markdown]
+# ### With extra comments added
 
 # %%
-cm_KNN
+## KNN
+print("Best scores from multi-lingual model:")
+print(best_score_knn)
+print("Optimum parameters:")
+print(best_params_knn)
+print("Best scores from French language model:")
+print(best_score_fr_knn)
+print("Optimum parameters:")
+print(best_params_fr_knn)
 
 # %%
-# Look at the results for the first code
-disp = ConfusionMatrixDisplay(confusion_matrix=cm_KNN[0])
-disp.plot()
+# Random Forest
+print("Best scores from multi-lingual model:")
+print(best_score_rf)
+print("Optimum parameters:")
+print(best_params_rf)
+print("Best scores from French language model:")
+print(best_score_fr_rf)
+print("Optimum parameters:")
+print(best_params_fr_rf)
+
+# %%
+## Decision Tree
+print("Best scores from multi-lingual model:")
+print(best_score_dt)
+print("Optimum parameters:")
+print(best_params_dt)
+print("Best scores from French language model:")
+print(best_score_fr_dt)
+print("Optimum parameters:")
+print(best_params_fr_dt)
+
+# %%
+## NB
+print("Best scores from multi-lingual model:")
+print(best_score_nb)
+print("Optimum parameters:")
+print(best_params_nb)
+print("Best scores from French language model:")
+print(best_score_fr_nb)
+print("Optimum parameters:")
+print(best_params_fr_nb)
+
+# %%
+## SVM
+print("Best scores from multi-lingual model:")
+print(best_score_svm)
+print("Optimum parameters:")
+print(best_params_svm)
+print("Best scores from French language model:")
+print(best_score_fr_svm)
+print("Optimum parameters:")
+print(best_params_fr_svm)
+
+# %% [markdown]
+# ### Predict on test set
+
+# %%
+y_pred_svm = search_svm.best_estimator_.predict(X_test_embeddings_fr)
+y_pred_knn = search_knn.best_estimator_.predict(X_test_embeddings_fr)
+y_pred_rf = search_rf.best_estimator_.predict(X_test_embeddings_fr)
+y_pred_dt = search_dt.best_estimator_.predict(X_test_embeddings_fr)
+y_pred_nb = search_nb.best_estimator_.predict(X_test_embeddings_fr)
+
+# %%
+y_test_update = []
+for item in y_test:
+    if item.sum() == 0:
+        item = np.append(item, 1)
+    else:
+        item = np.append(item, 0)
+    y_test_update.append(list(item))
+
+y_test_update = np.asarray(y_test_update)
+
+# %%
+y_pred_update = []
+for item in y_pred_svm:
+    if item.sum() == 0:
+        item = np.append(item, 1)
+    else:
+        item = np.append(item, 0)
+    y_pred_update.append(list(item))
+
+y_pred_update = np.asarray(y_pred_update)
+
+# %%
+y_pred_update
+
+# %%
+cm_svm = multilabel_confusion_matrix(y_test_update, y_pred_update)
+
+# %%
+cm_svm
+
+# %%
+# Create confusion matrix from the best performing models
+cm_svm = multilabel_confusion_matrix(y_test, y_pred_svm)
+cm_knn = multilabel_confusion_matrix(y_test, y_pred_knn)
+cm_rf = multilabel_confusion_matrix(y_test, y_pred_rf)
+cm_dt = multilabel_confusion_matrix(y_test, y_pred_dt)
+cm_nb = multilabel_confusion_matrix(y_test, y_pred_nb)
+
+# %%
+# %%capture
+save_cm_plots(cm_svm, "svm")
+save_cm_plots(cm_knn, "knn")
+save_cm_plots(cm_rf, "random_forest")
+save_cm_plots(cm_dt, "decision_tree")
+save_cm_plots(cm_nb, "naive_bayes")
+
+# %% [markdown]
+# ### Important words / features
+
+# %% [markdown]
+# Most common words in each prediction...
+
+# %%
+pred_proba = search_knn.best_estimator_.predict_proba(X_train_embeddings_fr)
+
+# %%
+code_lists = []
+for preds in pred_proba:
+    code_list = list(pd.DataFrame(preds)[1])
+    code_lists.append(code_list)
+
+preds_df = pd.DataFrame(np.column_stack(code_lists), columns=codes)
+
+# %%
+preds_df["comment"] = X_train.reset_index(drop=True)
+
+# %%
+preds_df.head(1)
+
+# %%
+from nltk.corpus import stopwords
+from itertools import chain
+from textwrap import wrap
+
+stop = stopwords.words("french")
+
+# %%
+counts = []
+for code in codes:
+    words = chain.from_iterable(
+        line.split() for line in preds_df[preds_df[code] >= 0.5]["comment"].str.lower()
+    )
+    count = Counter(word for word in words if word not in stop)
+    counts.append(count)
+
+# %%
+print(codes[7].replace("_", " "))
+
+# %%
+y = [count for tag, count in counts[7].most_common(20)]
+x = [tag for tag, count in counts[7].most_common(20)]
+
+plt.bar(x, y, color="crimson")
+title = "Term frequencies: " + codes[7].replace("_", " ")
+plt.title("\n".join(wrap(title, 60)), fontsize=14, pad=10)
+plt.ylabel("Frequency")
+plt.xticks(rotation=90)
+for i, (tag, count) in enumerate(counts[7].most_common(20)):
+    plt.text(
+        i,
+        count,
+        f" {count} ",
+        rotation=90,
+        ha="center",
+        va="top" if i < 10 else "bottom",
+        color="white" if i < 10 else "black",
+    )
+plt.tight_layout()  # change the whitespace such that all labels fit nicely
 plt.show()
-
-# %%
--- cci_cameroon/pipeline/data_splitting.py
+++ cci_cameroon/pipeline/data_splitting.py
@@ -34,6 +34,54 @@ w1 = gd.workshop_1
 w2 = gd.workshop_2
 
 # %%
+# Update to add to data getters
+ifrc_data = pd.read_excel(
+    f"{project_directory}/inputs/data/COVID_19 Community feedback_Cameroon.xlsx",
+    sheet_name="FEEDBACK DATA_DONNEES ",
+)
+
+# %%
+codes_remove = [
+    "Belief that some people/institutions are making money because of the disease",
+    "Belief that the outbreak has ended",
+    "Belief that disease does exist or is real",
+    "Belief that the disease does not exist in this region or country",
+    "Beliefs about hand washing or hand sanitizers",
+    "Beliefs about face masks",
+    "Beliefs about ways of transmission",
+    "Observations of non-compliance with health measures",
+]
+
+
+# %%
+def get_other_codes(df):
+    other_codes = df[
+        df["TYPE OF FEEDBACK_TYPE DE RETOUR D'INFORMATION"]
+        == "Rumors_beliefs_observations"
+    ].copy()
+    other_codes = other_codes[["CODE", "FEEDBACK COMMENT_COMMENTAIRE"]].copy()
+    other_codes.replace(r"^ +| +$", r"", regex=True, inplace=True)
+    other_codes.columns = ["code", "comment"]
+    other_codes.drop_duplicates(subset=["code", "comment"], inplace=True)
+    other_codes = other_codes.loc[~other_codes["code"].isin(codes_remove)].copy()
+    other_codes.dropna(how="any", inplace=True)
+    other_codes_sample = other_codes.sample(n=150, random_state=1)
+    other_codes_sample = pwd.translate_column(other_codes_sample, "comment")
+    other_codes_sample["code"] = "no code"
+    other_codes_sample = other_codes_sample.reset_index()
+    other_codes_sample = other_codes_sample.rename(columns={"index": "id"})
+    other_codes_sample["id"] = other_codes_sample.index + 3887
+    other_codes_sample.set_index("id", inplace=True)
+    return other_codes_sample
+
+
+# %%
+other_codes = get_other_codes(ifrc_data)
+
+# %%
+# other_codes.reset_index('')
+
+# %%
 # Sort to ensure the order is consistent each time before splitting
 w1.sort_values(by="id", inplace=True)
 w2.sort_values(by="id", inplace=True)
@@ -51,6 +99,24 @@ model_data = shuffle(model_data, random_state=1)
 X_train, X_test, y_train, y_test = train_test_split(
     model_data["comment"], model_data["category_id"], test_size=0.20, random_state=1
 )
+
+# %%
+# Split data
+other_train, other_test = train_test_split(
+    other_codes["comment"], test_size=0.20, random_state=1
+)
+
+# %%
+X_train
+
+# %%
+other_train
+
+# %%
+len(other_test)
+
+# %%
+X_train
 
 # %%
 print(X_train.shape)
@@ -81,5 +147,21 @@ y_test.to_excel(
     index=True,
     index_label="id",
 )
+
+# %%
+other_train.to_excel(
+    f"{project_directory}/outputs/data/data_for_modelling/no_response_train.xlsx",
+    index=True,
+    index_label="id",
+)
+
+other_test.to_excel(
+    f"{project_directory}/outputs/data/data_for_modelling/no_response_test.xlsx",
+    index=True,
+    index_label="id",
+)
+
+# %%
+other_train
 
 # %%
